fbbef1f4cfcea884c95f79b13fcc272fe1a69910
SineMag <snenhlanhlamagubane99@gmail.com>
2025-08-20 16:41:18 +0200

    Showing voters who're 18+ years from a given list.

--- App.js
+++ App.js
@@ -4,10 +4,10 @@ const collection = [2, 67, 5, 18, 33];
 const doubledCollection = collection.map(element => element * 2);
 console.log("Doubled Collection:", doubledCollection);
 
-//root element exists
+// root element
 const root = document.getElementById("root");
 
-// Heading
+// Heading for fav movies
 const heading = document.createElement("h1");
 heading.innerText = "My Favourite Movies";
 root.appendChild(heading);
@@ -29,33 +29,56 @@ collection.map(element => {
     }
 });
 
-
-
 // Use map to create a new array of t/f
 const checkResults = collection.map(number => number > 5);
 
-// Display results
-checkResults.map((result, index) => {
-    const p = document.createElement("p");
-    root.appendChild(p);
-});
-
-
 // // Display results
-// checkResults.filter((result, index) => {
+// checkResults.map((result, index) => {
 //     const p = document.createElement("p");
-//     p.innerText = ` ${collection}`;
+//     p.innerText = `${result}`;
 //     root.appendChild(p);
 // });
- 
 
+// Heading for numbers bigger than five...
+const headBigFive = document.createElement("h1");
+headBigFive.innerText = `Elements Bigger than 5`;
+root.appendChild(headBigFive);
 
-const biggerThanFive = collection.filter(function (currentValue) {
-  return currentValue > 5;
+// Filter elements > 5
+const biggerThanFive = collection.filter(num => num > 5);
+
+// Breaking each element
+biggerThanFive.forEach(num => { 
+    root.append(num); 
+    root.append(document.createElement('br')); 
 });
 
-biggerThanFive.forEach(biggerThanFive => { 
-    root.append(biggerThanFive); 
-    root.append(document.createElement('br')) })
+// Trainees
+const trainee = [
+    { Mpumi : 18 },
+    { Siya : 31 },
+    { Sabelo : 25 },
+    { Msizi : 16 },
+    { Melo : 13 },
+    { Sine : 42 }
+];
 
+// Heading for voters
+const votersHeading = document.createElement("h1");
+votersHeading.innerText = `Who can Vote:`;
+root.appendChild(votersHeading);
 
+// Filtered age.. >= 18
+const olderThan18 = trainee.filter(person => {
+  const age = Object.values(person)[0];
+  return age >= 18;
+});
+
+// Who can vote??
+olderThan18.forEach(person => { 
+    const name = Object.keys(person)[0]; 
+    const age = person[name];
+    const p = document.createElement("p");
+    p.innerText = `${name} (${age}) ✅`;
+    root.appendChild(p);
+});
